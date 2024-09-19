5da31ddad9d95bb008805de0cf55c465cfb233fe
WisteFinch <41848558+WisteFinch@users.noreply.github.com>
2024-09-19 15:03:12 +0800

    feat & fix:  stratagem related issues
    - add stratagem (Orbital Napalm Barrage)
    - fix wrong stratagem (Orbital EMS Strike)

--- call-for-stratagems/app/build.gradle.kts
+++ call-for-stratagems/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "indie.wistefinch.callforstratagems"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.3.1"
+        versionCode = 6
+        versionName = "0.3.2"
         // Remove unnecessary language to reduce size.
         resourceConfigurations += listOf("en", "zh", "ru", "fr", "es", "ar")
 
